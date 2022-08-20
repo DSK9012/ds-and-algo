f147f2d36372fa18c2b1a76f8cde3d2e9e46a413
DSK9012 <saikiran.dannana@gmail.com>
2022-08-20 09:17:10 +0530

    added frequency counter pattern

---
+++ problem-solving/patterns/frequency-counter/check-frequency-of-squares.js
@@ -0,0 +1,61 @@
+/* Problem - Checkout frequency of squares
+
+    checkFrequencyOfSquares([1,3,2], [4,1,9]) // true
+    checkFrequencyOfSquares([1,3,2], [4,1]) // false
+    checkFrequencyOfSquares([1,2], [4,1,4]) // true
+    
+*/
+
+function checkFrequencyOfSquares(arr1, arr2){
+    if(arr1.length !== arr2.length){
+        return false;
+    }
+    let frequencyCounter1 = {}
+    let frequencyCounter2 = {}
+    for(let val of arr1){
+        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+    }
+    for(let val of arr2){
+        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1        
+    }
+    console.log(frequencyCounter1);
+    console.log(frequencyCounter2);
+    for(let key in frequencyCounter1){
+        if(!(key ** 2 in frequencyCounter2)){
+            return false
+        }
+        if(frequencyCounter2[key ** 2] !== frequencyCounter1[key]){
+            return false
+        }
+    }
+    return true
+}
+
+checkFrequencyOfSquares([1,2,3,2,5], [9,1,4,4,11])
+
+function checkFrequencyOfSquares(arr1, arr2){
+  if(arr1.length !== arr2.length){
+      return false;
+  }
+  let frequencyCounter1 = {}
+  let frequencyCounter2 = {}
+  for(let val of arr1){
+      frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+  }
+  for(let val of arr2){
+      frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1        
+  }
+  console.log(frequencyCounter1);
+  console.log(frequencyCounter2);
+  for(let key in frequencyCounter1){
+      if(!(key ** 2 in frequencyCounter2)){
+          return false
+      }
+      if(frequencyCounter2[key ** 2] !== frequencyCounter1[key]){
+          return false
+      }
+  }
+  return true
+}
+
+checkFrequencyOfSquares([1,2,3,2,5], [9,1,4,4,11])
